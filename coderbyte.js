501c213d05699ee4b87ccc87c4ea3d4937ff995f
Mahima GP <mahima.srikanta@gmail.com>
2017-01-09 19:16:19 -0800

    coder
    coder

--- coderbyte.js
+++ coderbyte.js
@@ -336,3 +336,160 @@ function ChangingSequence(arr){
 }
 
 ChangingSequence([-4, -2, 9, 10])
+/*Challenge
+Using the JavaScript language, have the function OverlappingRanges(arr) take the array of numbers stored in arr which will contain 5 positive integers, the first two representing a range of numbers (a to b), the next 2 also representing another range of integers (c to d), and a final 5th element (x) which will also be a positive integer, and return the string true if both sets of ranges overlap by at least x numbers. For example: if arr is [4, 10, 2, 6, 3] then your program should return the string true. The first range of numbers are 4, 5, 6, 7, 8, 9, 10 and the second range of numbers are 2, 3, 4, 5, 6. The last element in the array is 3, and there are 3 numbers that overlap between both ranges: 4, 5, and 6. If both ranges do not overlap by at least x numbers, then your program should return the string false. 
+Sample Test Cases
+Input:5,11,1,5,1
+Output:true
+
+Input:1,8,2,4,4
+Output:false
+*/
+
+function OverlappingRanges(arr){
+  var arr1=[];
+  var arr2 =[];
+  
+  for(var i=arr[0];i<= arr[1];i++){
+    arr1.push(i);
+  }
+  for(var j=arr[2];j<= arr[3];j++){
+    arr2.push(j);
+  }
+ var result= arr1.filter(function(num){
+    return arr2.includes(num)
+  });
+  
+  return result.length >= arr[4] ? true:false
+}
+
+OverlappingRanges([1,2,1,3,1])
+
+/*Challenge
+Using the JavaScript language, have the function Superincreasing(arr) take the array of numbers stored in arr and determine if the array forms a superincreasing sequence where each element in the array is greater than the sum of all previous elements. The array will only consist of positive integers. For example: if arr is [1, 3, 6, 13, 54] then your program should return the string "true" because it forms a superincreasing sequence. If a superincreasing sequence isn't formed, then your program should return the string "false" 
+Sample Test Cases
+Input:1,2,3,4
+Output:"false"
+
+Input:1,2,5,10
+Output:"true"
+*/
+
+function Superincreasing(arr){
+  for(var i =0;i<arr.length-1;i++){
+    if(arr[i+1]<=(arr[i]+arr[i-1])){
+      return false;
+    }
+  }
+  return true;
+}
+ Superincreasing([1,2,5,10]);
+ 
+/*Challenge
+Using the JavaScript language, have the function HammingDistance(strArr) take the array of strings stored in strArr, which will only contain two strings of equal length and return the Hamming distance between them. The Hamming distance is the number of positions where the corresponding characters are different. For example: if strArr is ["coder", "codec"] then your program should return 1. The string will always be of equal length and will only contain lowercase characters from the alphabet and numbers. 
+Sample Test Cases
+Input:"10011", "10100"
+Output:3
+
+Input:"helloworld", "worldhello"
+Output:8
+*/
+
+function HammingDistance(strArr){
+  var result=0;
+  for(var i=0;i<strArr[0].length;i++){
+    if(strArr[0][i] !== strArr[1][i]){
+      result++;
+    }
+  }
+  return result;
+}
+
+HammingDistance(["helloworld", "worldhello"])
+
+/*Challenge
+Using the JavaScript language, have the function RectangleArea(strArr) take the array of strings stored in strArr, which will only contain 4 elements and be in the form (x y) where x and y are both integers, and return the area of the rectangle formed by the 4 points on a Cartesian grid. The 4 elements will be in arbitrary order. For example: if strArr is ["(0 0)", "(3 0)", "(0 2)", "(3 2)"] then your program should return 6 because the width of the rectangle is 3 and the height is 2 and the area of a rectangle is equal to the width * height. 
+Sample Test Cases
+Input:"(1 1)","(1 3)","(3 1)","(3 3)"
+Output:4
+
+Input:"(0 0)","(1 0)","(1 1)","(0 1)"
+Output:1
+*/
+function RectangleArea(strArr){
+  var area=[];
+  for(var i=0;i<strArr.length;i++){
+    var result =strArr[i].match(/[0-9]/g);
+    if(result[0] !== result[1]){
+      
+      area.push(result[0]-result[1])
+    }
+
+    
+  }
+  console.log(area);
+  return area.reduce(function(a,c){
+    return Math.abs(a*c);
+  })
+}
+RectangleArea(["(0 0)","(5 0)","(0 3)","(5 3)"])
+
+/*
+Challenge
+Using the JavaScript language, have the function BitwiseOne(strArr) take the array of strings stored in strArr, which will only contain two strings of equal length that represent binary numbers, and return a final binary string that performed the bitwise OR operation on both strings. A bitwise OR operation places a 0 in the new string where there are zeroes in both binary strings, otherwise it places a 1 in that spot. For example: if strArr is ["1001", "0100"] then your program should return the string "1101" 
+Sample Test Cases
+Input:"100", "000"
+Output:"100"
+
+Input:"00011", "01010"
+Output:"01011"
+*/
+function BitwiseOne(strArr) {
+  var num1 = strArr[0];
+  var num2 = strArr[1];
+  var result="";
+  for(var i =0;i<num1.length;i++){
+    if((num1[i]==="1") || (num2[i]==="1")){
+      result+="1";
+    }
+    else{
+      result+="0";
+    }
+  }
+  return result;
+}
+BitwiseOne(["00011", "01010"]);
+
+/*Challenge
+Using the JavaScript language, have the function OtherProducts(arr) take the array of numbers stored in arr and return a new list of the products of all the other numbers in the array for each element. For example: if arr is [1, 2, 3, 4, 5] then the new array, where each location in the new array is the product of all other elements, is [120, 60, 40, 30, 24]. The following calculations were performed to get this answer: [(2*3*4*5), (1*3*4*5), (1*2*4*5), (1*2*3*5), (1*2*3*4)]. You should generate this new array and then return the numbers as a string joined by a hyphen: 120-60-40-30-24. The array will contain at most 10 elements and at least 1 element of only positive integers. 
+Sample Test Cases
+Input:1,4,3
+Output:"12-3-4"
+
+Input:3,1,2,6
+Output:"12-36-18-6"
+*/
+
+function OtherProducts(arr){
+  var result="";
+  var product= arr.reduce(function(a,c){
+    return a*c;
+  });
+  
+  for(var i =0;i<arr.length;i++){
+    result+=(product/arr[i]) +"-";
+  }
+  return result.slice(0,-1);
+}
+OtherProducts([3,1,2,6]);
+
+/*
+Challenge
+Using the JavaScript language, have the function WaveSorting(arr) take the array of positive integers stored in arr and return the string true if the numbers can be arranged in a wave pattern: a1 > a2 < a3 > a4 < a5 > ..., otherwise return the string false. For example, if arr is: [0, 1, 2, 4, 1, 4], then a possible wave ordering of the numbers is: [2, 0, 4, 1, 4, 1]. So for this input your program should return the string true. The input array will always contain at least 2 elements. More examples are given below as sample test cases. 
+Sample Test Cases
+Input:0, 1, 2, 4, 1, 1, 1
+Output:"false"
+
+Input:0, 4, 22, 4, 14, 4, 2
+Output:"true"
+*/
